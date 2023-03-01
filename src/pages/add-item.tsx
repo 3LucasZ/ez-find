968fea336d9c84171d952dd5ed0c5c95fa257a26
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2023-02-28 23:13:29 -0800

    revised add item page

--- src/pages/add-item.tsx
+++ src/pages/add-item.tsx
@@ -1,12 +1,29 @@
 import React, { useState } from "react";
 import Router from "next/router";
 
+import {
+  FormControl,
+  Input,
+  Button,
+  
+} from '@chakra-ui/react'
+import { Form } from "react-bootstrap";
+
+enum FormState {
+  Input,
+  Submitting,
+  Error,
+  Success
+}
+
 const ItemDraft: React.FC = () => {
-  const [name, setName] = useState("");
-  const [locations, addLocation] = useState([]);
+  const [name, setItemName] = useState("");
+  const [formState, setFormState] = useState(FormState.Input);
+  const [locations, setLocation] = useState("");
 
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
+    setFormState(FormState.Submitting);
     try {
       const body = { name, locations };
       const res = await fetch("/api/add-item", {
@@ -15,27 +32,53 @@ const ItemDraft: React.FC = () => {
         body: JSON.stringify(body),
       });
       await Router.push("view-items");
+      setFormState(FormState.Success);
     } catch (error) {
+      setFormState(FormState.Error);
       console.error(error);
     }
   };
+/* Use to unit test form states */
+  // const simulateSubmissionData = async (e: React.SyntheticEvent) => {
+  //   console.log("Hello");
+  //   e.preventDefault();
+  //   setFormState(FormState.Submitting);
+  //   setTimeout(() => {
+  //     console.log("Sending");
+  //     setFormState(FormState.Input);
+  //   }, 3000);
+  // }
 
   return (
+    <div className="add-item-form">
     <form onSubmit={submitData}>
-      <h1>Create Item</h1>
-      <input
-        autoFocus
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Name"
-        type="text"
-        value={name}
-      />
+      <FormControl> 
+        <Input 
+          variant="filled"
+          marginTop={10}
+          placeholder="Item name"
+          isDisabled={formState===FormState.Input ? false : true}
+          onChange={(e) => setItemName(e.target.value)}>
+        </Input>
+        <Input 
+          variant="filled"
+          marginY={10}
+          placeholder="Item Location"
+          isDisabled={formState===FormState.Input ? false : true}
+          onChange={(e) => setLocation(e.target.value)}>
+        </Input>
+        <Button
+          mt={4}
+          size='lg'
+          colorScheme="teal"
+          type='submit'
+          isLoading={formState == FormState.Input ? false: true}>
 
-      <input disabled={!name} type="submit" value="Update" />
-      <a href="#" onClick={() => Router.push("/")}>
-        or Cancel
-      </a>
+        Submit Item
+      </Button>
+      </FormControl>
     </form>
+    </div>
   );
 };
 
