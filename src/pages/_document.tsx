295c25e4b7fd9543ba7df0ff1cab9d384b7146c6
3LucasZ <lucas.zheng@warriorlife.net>
2023-09-29 21:02:43 -0700

    add js to document tsx?

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script type="application/javascript" src="services/dymo.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
