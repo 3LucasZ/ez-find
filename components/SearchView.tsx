a85e47006e2711b0b3c5abc410628c43c1184d1b
3LucasZ <lucas.zheng@warriorlife.net>
2023-10-19 21:00:30 -0700

    Create SearchView.tsx

---
+++ components/SearchView.tsx
@@ -0,0 +1,60 @@
+import { Box, HStack, Input, Stack } from "@chakra-ui/react";
+import { ReactNode, useState } from "react";
+
+type SearchViewProps = {
+  set: PairProps[];
+};
+type PairProps = {
+  name: string;
+  widget: ReactNode;
+};
+type StateProps = {
+  query: string;
+  subset: PairProps[];
+};
+export default function ConfirmDeleteModal(props: SearchViewProps) {
+  props.set.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  const [state, setState] = useState<StateProps>({
+    query: "",
+    subset: props.set,
+  });
+  const handleSearchQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const res = props.set.filter((pair) => {
+      if (e.target.value === "") return props.set;
+      return pair.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setState({
+      query: e.target.value,
+      subset: res,
+    });
+  };
+  return (
+    <div>
+      <Box pl="25vw" pr="25vw">
+        <Input
+          variant="filled"
+          placeholder="Search"
+          type="search"
+          value={state.query}
+          onChange={handleSearchQueryChange}
+        />
+      </Box>
+      <Box h="4vh"></Box>
+      <HStack maxH="100%">
+        <Box w="100%" />
+        <Box overflowY="auto" maxH="100%" w="100%">
+          <Stack w="100%">{state.subset.map((pair) => pair.widget)}</Stack>
+        </Box>
+        <Box w="100%" />
+      </HStack>
+    </div>
+  );
+}
